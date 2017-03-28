c8644b3d3d49ba0eef00fe8aad74db6c705fce93
Jackie Tieu <jackietieu2@gmail.com>
2017-03-27 19:38:54 -0700

    completed beer-song

--- beer-song/babel-output/beer-song.js
+++ beer-song/babel-output/beer-song.js
@@ -1,5 +1,9 @@
 'use strict';
 
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
@@ -12,13 +16,9 @@ var Beer = (function () {
   _createClass(Beer, [{
     key: 'verse',
     value: function verse(bottles) {
-      result = '';
-
-      if (bottles > 0) {
-        result = bottles + ' bottles of beer on the wall, ' + bottles + ' bottles of beer.\n      Take ' + (bottles === 1 ? 'it' : 'one') + ' down and pass it around, ' + (bottles - 1) + ' bottles of beer on the wall.';
-      } else {
-        result = 'No more bottles of beer on the wall, no more bottles of beer.\n      Go to the store and buy some more, 99 bottles of beer on the wall.';
-      }
+      var beerOnTheWall = (bottles || 'No more') + ' ' + (bottles === 1 ? 'bottle' : 'bottles'),
+          passOrBuy = bottles === 0 ? 'Go to the store and buy some more' : 'Take ' + (bottles === 1 ? 'it' : 'one') + ' down and pass it around',
+          result = beerOnTheWall + ' of beer on the wall, ' + beerOnTheWall.toLowerCase() + ' of beer.\n' + passOrBuy + ', ' + (bottles ? bottles - 1 || 'no more' : '99') + ' ' + (bottles - 1 === 1 ? 'bottle' : 'bottles') + ' of beer on the wall.\n';
 
       return result;
     }
@@ -28,13 +28,17 @@ var Beer = (function () {
       var startBottles = arguments.length <= 0 || arguments[0] === undefined ? 99 : arguments[0];
       var endBottles = arguments.length <= 1 || arguments[1] === undefined ? 0 : arguments[1];
 
+      var verses = [];
       for (var i = startBottles; i >= endBottles; i--) {
-        this.verse(i);
+        verses.push(this.verse(i));
       }
+
+      return verses.join('\n');
     }
   }]);
 
   return Beer;
 })();
 
-module.exports = Beer;
+exports['default'] = new Beer();
+module.exports = exports['default'];
--- beer-song/beer-song.js
+++ beer-song/beer-song.js
@@ -1,25 +1,23 @@
 class Beer {
   verse(bottles) {
-    result = '';
-
-    if (bottles > 0) {
-      result = `${bottles} bottles of beer on the wall, ${bottles} bottles of beer.
-      Take ${bottles === 1 ? 'it' : 'one'} down and pass it around, ${bottles - 1} bottles of beer on the wall.`;
-    } else {
-      result = `No more bottles of beer on the wall, no more bottles of beer.
-      Go to the store and buy some more, 99 bottles of beer on the wall.`;
-    }
+    let beerOnTheWall = `${bottles || 'No more'} ${bottles === 1 ? 'bottle' : 'bottles'}`,
+        passOrBuy = bottles === 0 ?
+        `Go to the store and buy some more` :
+        `Take ${bottles === 1 ? 'it' : 'one'} down and pass it around`,
+        result = `${beerOnTheWall} of beer on the wall, ${beerOnTheWall.toLowerCase()} of beer.
+${passOrBuy}, ${bottles ? bottles - 1 || 'no more' : '99'} ${bottles - 1 === 1 ? 'bottle' : 'bottles'} of beer on the wall.\n`;
 
     return result;
   }
 
   sing(startBottles = 99, endBottles = 0) {
+    let verses = [];
     for (let i = startBottles; i >= endBottles; i--) {
-      this.verse(i);
+      verses.push(this.verse(i));
     }
+
+    return verses.join('\n');
   }
 }
 
-
-
-module.exports = Beer;
+export default new Beer;

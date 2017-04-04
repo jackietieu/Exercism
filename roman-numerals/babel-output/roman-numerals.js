4121245d2fb417d0c8f35e777b973963ca89ee85
Jackie Tieu <jackietieu2@gmail.com>
2017-04-03 23:09:17 -0700

    refactored roman-numerals

--- roman-numerals/babel-output/roman-numerals.js
+++ roman-numerals/babel-output/roman-numerals.js
@@ -7,9 +7,13 @@ Object.defineProperty(exports, '__esModule', {
 var _slicedToArray = (function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i['return']) _i['return'](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError('Invalid attempt to destructure non-iterable instance'); } }; })();
 
 var LETTER_VALUES = {
-  I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000
+  I: 1, V: 5, X: 10, L: 50,
+  C: 100, D: 500, M: 1000
 },
-    LETTERS = Object.keys(LETTER_VALUES);
+    LETTERS = Object.keys(LETTER_VALUES),
+    inRange = function inRange(min, max, val) {
+  return min <= val && val < max;
+};
 
 exports['default'] = function (num) {
   var translation = '',
@@ -23,26 +27,22 @@ exports['default'] = function (num) {
 
     var twoLettersDown = LETTERS[LETTERS.indexOf(letter) - 2],
         oneLetterDown = LETTERS[LETTERS.indexOf(letter) - 1],
+        oneLetterUp = LETTERS[LETTERS.indexOf(letter) + 1],
+        twoValuesDown = LETTER_VALUES[twoLettersDown],
         oneValueDown = LETTER_VALUES[oneLetterDown];
 
-    if ((val === 50 || val === 500) && num >= val - oneValueDown && num < val) {
+    if ((val === 50 || val === 500) && inRange(val - oneValueDown, val, num)) {
       translation += oneLetterDown + letter;
       num -= val - LETTER_VALUES[oneLetterDown];
-    } else if (twoLettersDown && num < val && num >= val - LETTER_VALUES[LETTERS[LETTERS.indexOf(letter) - 2]]) {
+    } else if (twoLettersDown && inRange(val - twoValuesDown, val, num)) {
       translation += twoLettersDown + letter;
       num -= val - LETTER_VALUES[twoLettersDown];
     } else if (num / val >= 1) {
       rollover = num / val;
       num %= val;
+      translation += rollover === 4 ? letter + oneLetterUp : letter.repeat(rollover);
+      rollover = 0;
     }
-
-    if (rollover !== 4) {
-      translation += letter.repeat(rollover);
-    } else {
-      translation += letter + LETTERS[LETTERS.indexOf(letter) + 1];
-    }
-
-    rollover = 0;
   });
 
   return translation;
--- roman-numerals/roman-numerals.js
+++ roman-numerals/roman-numerals.js
@@ -1,7 +1,9 @@
 const LETTER_VALUES = {
-  I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000
-},
-LETTERS = Object.keys(LETTER_VALUES);
+        I: 1, V: 5, X: 10, L: 50,
+        C: 100, D: 500, M: 1000
+      },
+      LETTERS = Object.keys(LETTER_VALUES),
+      inRange = (min, max, val) => min <= val && val < max;
 
 export default (num) => {
   let translation = '',
@@ -10,26 +12,22 @@ export default (num) => {
   Object.entries(LETTER_VALUES).reverse().forEach(([letter, val]) => {
     let twoLettersDown = LETTERS[LETTERS.indexOf(letter) - 2],
         oneLetterDown = LETTERS[LETTERS.indexOf(letter) - 1],
+        oneLetterUp = LETTERS[LETTERS.indexOf(letter) + 1],
+        twoValuesDown = LETTER_VALUES[twoLettersDown],
         oneValueDown = LETTER_VALUES[oneLetterDown];
 
-    if ((val === 50 || val === 500) && (num >= val - oneValueDown) && (num < val)) {
+    if ((val === 50 || val === 500) && inRange(val - oneValueDown, val, num)) {
       translation += oneLetterDown + letter;
       num -= (val - LETTER_VALUES[oneLetterDown]);
-    } else if (twoLettersDown && (num < val) && (num >= (val - LETTER_VALUES[LETTERS[LETTERS.indexOf(letter) - 2]]))) {
+    } else if (twoLettersDown && inRange(val - twoValuesDown, val, num)) {
       translation += twoLettersDown + letter;
       num -= (val - LETTER_VALUES[twoLettersDown]);
     } else if (num / val >= 1) {
       rollover = num / val;
       num %= val;
+      translation += rollover === 4 ? letter + oneLetterUp : letter.repeat(rollover);
+      rollover = 0;
     }
-
-    if (rollover !== 4) {
-      translation += letter.repeat(rollover);
-    } else {
-      translation += letter + LETTERS[LETTERS.indexOf(letter) + 1];
-    }
-
-    rollover = 0;
   });
 
   return translation;

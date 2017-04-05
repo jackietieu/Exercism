24e04a918dfc64e622411c41f9727b4033a5ad92
Jackie Tieu <jackietieu2@gmail.com>
2017-04-04 22:07:39 -0700

    completed prime-factors

---
+++ prime-factors/babel-output/prime-factors.js
@@ -0,0 +1,28 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = primeFactors;
+
+function primeFactors(num) {
+  return {
+    "for": function _for(num) {
+      var factors = [],
+          currentFactor = 2;
+
+      while (num > 1) {
+        if (num % currentFactor === 0) {
+          factors.push(currentFactor);
+          num /= currentFactor;
+        } else {
+          currentFactor++;
+        }
+      }
+
+      return factors;
+    }
+  };
+}
+
+module.exports = exports["default"];
---
+++ prime-factors/prime-factors.js
@@ -0,0 +1,19 @@
+export default function primeFactors(num) {
+  return {
+    for: (num) => {
+      let factors = [],
+          currentFactor = 2;
+
+      while (num > 1) {
+        if (num % currentFactor === 0) {
+          factors.push(currentFactor);
+          num /= currentFactor;
+        } else {
+          currentFactor++;
+        }
+      }
+
+      return factors;
+    }
+  }
+}

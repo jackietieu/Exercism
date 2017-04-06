1ecd0c8cb51829a949a3f09b6354ce7aab26cb68
Jackie Tieu <jackietieu2@gmail.com>
2017-04-05 21:29:27 -0700

    completed allergens

--- allergies/allergies.js
+++ allergies/allergies.js
@@ -1,45 +1,24 @@
-const ALLERGIES = {
-  1: 'eggs',
-  2: 'peanuts',
-  4: 'shellfish',
-  8: 'strawberries',
-  16: 'tomatoes',
-  32: 'chocolate',
-  64: 'pollen',
-  128: 'cats'
-}
-
-function allergenValues(max) {
-  let values = [1];
-
-  while (values.slice(-1)[0] < max) {
-    values.push(Math.pow(2, values.length));
-  }
-
-  return values;
-}
-
+const ALLERGENS = [
+  'cats',
+  'pollen',
+  'chocolate',
+  'tomatoes',
+  'strawberries',
+  'shellfish',
+  'peanuts',
+  'eggs'
+]
 
 export default function(score) {
-  this.list: () => {
-    let possibleValues = allergenValues(score),
-        result = [];
-
-    if (ALLERGIES[score]) {
-      result.push(ALLERGIES[score]);
+  this.score = score % 256;
+  this.list = () => ALLERGENS.filter((allergen, i) => {
+    let currentScore = 2**(7 - i);
+    if (this.score === currentScore || currentScore < this.score) {
+      this.score -= currentScore;
+      return true;
     } else {
-      possibleValues.reverse().forEach(value => {
-        if (score - value >= 0 && ALLERGIES[value]) {
-          score -= value;
-          result.push(ALLERGIES[value]);
-        }
-      })
+      return false;
     }
-
-    return result.reverse();
-  }
-  this.allergicTo: (allergen) => {
-    let allergenList = this.list();
-    return allergenList.includes(allergen);
-  }
+  }).reverse();
+  this.allergicTo = allergen => this.list().includes(allergen);
 }
